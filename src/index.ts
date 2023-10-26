108d64fabcd046bcc559e2d6ebcaf9d18bdb26c4
Phillip Mwaniki Nzuli <codezeroprivate@gmail.com>
2023-10-26 10:49:33 +0300

    database setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import * as dotenv from 'dotenv';
 import app from './app';
+import { AppDataSource } from "./database/data-source";
 
 dotenv.config();
 
 const PORT = process.env.APP_PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+AppDataSource.initialize().then(async () => {
+    console.log('Database connection successful');
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+
+}).catch((err) => console.error(err));
