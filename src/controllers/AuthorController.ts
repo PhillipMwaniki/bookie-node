b5f611992a3c4ecf2d8d36b64357ebe5bc636161
Phillip Mwaniki Nzuli <codezeroprivate@gmail.com>
2023-11-01 00:41:20 +0300

    added paginator

--- src/controllers/AuthorController.ts
+++ src/controllers/AuthorController.ts
@@ -2,12 +2,13 @@ import { Request, Response } from 'express';
 import { AppDataSource } from '../database/data-source';
 import { Author } from '../entities/Author';
 import { ResponseUtil } from '../../utils/Response';
+import { Paginator } from '../database/Paginator';
 
 export default class AuthorController {
    async getAuthors(req: Request, res: Response) {
-      const authors = await AppDataSource.getRepository(Author).find();
-
-      return ResponseUtil.sendSuccessfulResponse<Author[]>(res, "Fetch Authors Successful", authors)
+      const builder = await AppDataSource.getRepository(Author).createQueryBuilder().orderBy("id", "DESC");
+      const { records: authors, paginationInfo } = await Paginator.paginate(builder, req, res);
+      return ResponseUtil.sendSuccessfulResponse<Author[]>(res, "Fetch Authors Successful", authors, paginationInfo);
    }
 
    async getAuthor(req: Request, res: Response) {
---
+++ src/database/Paginator.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { ObjectLiteral, SelectQueryBuilder } from "typeorm";
+
+interface PaginationInfo {
+    currentPage: number;
+    pageSize: number;
+    totalItems: number;
+    pages: number;
+    hasNext: boolean;
+    hasPrev: boolean;
+}
+
+export class Paginator {
+    static async paginate<T extends ObjectLiteral>(queryBuilder: SelectQueryBuilder<T>, req: Request, res: Response) {
+        let page = Number(req.query.page) || 1;
+        let pageSize = Number(req.query.pageSize) || 15;
+        const offset = (page - 1) * pageSize;
+
+        const records = await queryBuilder.skip(offset).take(pageSize).getMany();
+        const totalItems = await queryBuilder.getCount();
+        const pages = Math.ceil(totalItems / pageSize);
+        const currentPage = offset / pageSize + 1;
+        const hasNext = currentPage < pages;
+        const hasPrev = currentPage > 1;
+
+        const paginationInfo: PaginationInfo = {
+            currentPage,
+            pageSize,
+            totalItems,
+            pages,
+            hasNext,
+            hasPrev
+        };
+
+        return { records, paginationInfo };
+    }
+}
