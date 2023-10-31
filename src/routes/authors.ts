0ebef4d069ee7bfe21a2790cf2a3bae4e4b60ccb
Phillip Mwaniki Nzuli <codezeroprivate@gmail.com>
2023-10-31 23:58:47 +0300

    added error handling

--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import AuthorController from '../controllers/AuthorController';
+import { ErrorHandler } from '../../utils/ErrorHandler';
 
 const router = express.Router();
 
 const authorController = new AuthorController();
 
-router.get("/", authorController.getAuthors);
-router.get("/:id", authorController.getAuthor);
+router.get("/", ErrorHandler.handleErrors(authorController.getAuthors));
+router.get("/:id", ErrorHandler.handleErrors(authorController.getAuthor));
 
 
 export default router;
